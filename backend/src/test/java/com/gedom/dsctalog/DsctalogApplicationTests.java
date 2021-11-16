bc84066b456b31a1e2ed12555be685f37ebb8d4d
gedom2012 <gedom2012@gmail.com>
2021-11-16 23:10:43 +0000

    firs commit

---
+++ backend/src/test/java/com/gedom/dsctalog/DsctalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gedom.dsctalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsctalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
