ce7930586ac802f9620abe2a9a330ca4d9bc0c36
gedom2012 <gedom2012@gmail.com>
2021-11-20 01:19:07 +0000

    Find Category by id and added exception handling

--- backend/src/main/java/com/gedom/dsctalog/services/CategoryService.java
+++ backend/src/main/java/com/gedom/dsctalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.gedom.dsctalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.gedom.dsctalog.DTO.CategoryDTO;
 import com.gedom.dsctalog.entities.Category;
 import com.gedom.dsctalog.repositories.CategoryRepository;
+import com.gedom.dsctalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,6 +24,13 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) throws Exception {
+		Optional<Category> obj = repository.findById(id);	
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);		
 	}
 
 }
