ef624f592da3710a1de914abdd3f76ff3b19237a
gedom2012 <gedom2012@gmail.com>
2021-11-27 09:12:01 +0000

    Pagination

--- backend/src/main/java/com/gedom/dsctalog/services/CategoryService.java
+++ backend/src/main/java/com/gedom/dsctalog/services/CategoryService.java
@@ -1,14 +1,14 @@
 package com.gedom.dsctalog.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,10 +25,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true) // improvement performance to transactions of read
-	public List<CategoryDTO> findAll() {
-		List<Category> list = repository.findAll();
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-
+	public Page<CategoryDTO> findAllPaged(PageRequest page) {
+		Page<Category> list = repository.findAll(page);
+		return list.map(x -> new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true)
