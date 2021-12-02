64eed821c4df2204e86cc3966f2187853211545b
gedom2012 <gedom2012@gmail.com>
2021-12-02 23:07:52 +0000

    Update ProductRepositoyTests.java

--- backend/src/test/java/com/gedom/dsctalog/repositories/ProductRepositoyTests.java
+++ backend/src/test/java/com/gedom/dsctalog/repositories/ProductRepositoyTests.java
@@ -1,10 +1,13 @@
 package com.gedom.dsctalog.repositories;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,10 +21,16 @@ public class ProductRepositoyTests {
 	@Autowired
 	private ProductRepository repository;
 
+	private Long existingId, nonExistingId;
+
+	@BeforeEach
+	public void setup() {
+		existingId = 1L;
+		nonExistingId = 99999L;
+	}
+
 	@Test
 	public void deleteShouldDeleteObjectWhenExists() {
-		Long existingId = 1L;
-
 		repository.deleteById(existingId);
 
 		Optional<Product> result = repository.findById(existingId);
@@ -31,12 +40,22 @@ public class ProductRepositoyTests {
 
 	@Test
 	public void deleteShouldEmptyResultDataAccessExceptionWhenDoesNotExists() {
-		Long nonExistingId = 99999L;
-
 		assertThrows(EmptyResultDataAccessException.class, () -> {
 			repository.deleteById(nonExistingId);
 		});
 
+	}
+
+	@Test
+	public void returnsProductWhenObjectExists() {
+		Optional<Product> result = repository.findById(existingId);
+		assertNotNull(result);
+	}
+
+	@Test
+	public void returnsEmptyWhenObjectDoNotExists() {
+		Optional<Product> result = repository.findById(nonExistingId);
+		assertTrue(result.isEmpty());
 	}
 
 }
